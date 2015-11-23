e524d2f4ee55052a674b086f1d1417514f038f58
Jordi Lopez <jordi.lopez@estudiant.upc.edu>
2015-11-23 17:21:17 +0100

    sql changed

--- sql/beeterdb-backup.sql
+++ sql/beeterdb-backup.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: beeterdb
 -- ------------------------------------------------------
--- Server version	5.6.26-log
+-- Server version	5.6.27-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
